abf161cae897cfaa63c2973bc52f575d44d11359
Moslem-Elo <moslemeo@icloud.com>
2024-01-11 02:46:16 +0100

    Design anpassungen.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,85 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div>
+        <header>
+            <div class="wrapper">
+                <div></div>
+                <nav>
+                    <router-link to="/" class="custom-link" :class="{ 'active-link': $route.path === '/' }">Home</router-link>
+                    <router-link to="/about" class="custom-link" :class="{ 'active-link': $route.path === '/about' }">Service</router-link>
+                </nav>
+            </div>
+        </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <body>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        </body>
     </div>
-  </header>
-
-  <RouterView />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    background-color: #413f3f;
+    padding: 30px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 999;
+
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+    margin: 0; /* Setze den Seitenrand auf 0, um den Standardseitenrand zu entfernen */
+    background-color: #03daf8; /* Hintergrundfarbe des Body-Elements */
+    background-size: cover; /* Skaliert das Hintergrundbild, um die gesamte Fläche zu bedecken */
+}
+
+
+.image-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    z-index: -1; /* Setze das Bild hinter den anderen Elementen */
+}
+
+.image-container img {
+    width: 100vw; /* Breite des Viewports */
+    height: 100vh; /* Höhe des Viewports */
+    object-fit: cover; /* Skaliert das Bild, um die gesamte Fläche zu bedecken */
+    object-position: center; /* Position des Bildes im Container */
+}
+
+.wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 30px;
+    font-family: Calibri;
+    font-style: normal;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+    gap: 20px;
 }
+
+
+.custom-link {
+    color: #fffafa; /* Farbe für nicht aktive Links */
+}
+
+.active-link {
+    color: #4f6cff; /* Farbe für aktive Links */
+    /* Hier könntest du auch andere Stile für aktive Links hinzufügen */
+}
+
+
+
 </style>
